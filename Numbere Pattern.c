3412af40eb6759032cbb25892c74e054f9868f3f
Lakshmikavya123 <157302606+Lakshmikavya123@users.noreply.github.com>
2024-02-08 22:28:59 +0530

    Create Numbere Pattern.c

---
+++ Numbere Pattern.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+int val=1;
+for(int row=1;row<=n;row++)
+{
+for(int col=1;col<=n;col++)
+{
+printf("%02d",val++);
+}
+printf("\n");
+}
+return 0;
+}
