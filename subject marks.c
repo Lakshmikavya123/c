f6d5ea00c01989181b3b18d5c6f0350eda51d115
Lakshmikavya123 <157302606+Lakshmikavya123@users.noreply.github.com>
2024-02-21 19:26:53 +0530

    Create subject marks.c

---
+++ subject marks.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int s_no;
+scanf("%d",&s_no);
+int subjects;
+scanf("%d",&subjects);
+int marks[subjects];
+for(int i=0;i<subjects;i++)
+{
+scanf("%d",&marks[i]);
+}
+for(int i=0;i<subjects;i++);
+{
+scanf("%d",&marks[i]);
+}
+return 0;
+}
