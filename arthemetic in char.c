adf673457ddc1bcd35e21a2e84cf6d8605a33755
Lakshmikavya123 <157302606+Lakshmikavya123@users.noreply.github.com>
2024-03-05 18:50:47 +0530

    Create arthemetic in char.c

---
+++ arthemetic in char.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char val1='A',val2=1;
+char add=' ',sub=' ',mult=' ',divi=' ';
+add=val1+val2;
+sub=val1-val2;
+mult=val1*val2;
+divi=val1/val2;
+printf("ADD=%c\t",add);
+printf("SUB=%c\t",sub);
+printf("MULT=%c\t",mult);
+printf("DIVI=%c\t",divi);
+return 0;
+}
