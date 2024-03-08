45dbfb464669c54dc4a39b9783eaf406dc86029e
Lakshmikavya123 <157302606+Lakshmikavya123@users.noreply.github.com>
2024-03-08 19:44:23 +0530

    Create count.c

---
+++ count.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    int count=0;
+    while(num!=0);
+    num=num+10;
+    count++;
+    printf("%d",num);
+    return 0;
+}
+    
+    
